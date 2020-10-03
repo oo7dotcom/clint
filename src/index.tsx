992e8d02891c458cb0f9b73583241b0246047a2d
Henrique Cunha <henriquecunhawd@gmail.com>
2020-10-03 00:58:16 -0300

    [BLESSED_REACT_AND_SQS]: Clear index

--- src/index.tsx
+++ src/index.tsx
@@ -4,41 +4,20 @@ import * as blessed from "blessed";
 import SQS from "./provider/aws/sqs";
 
 class App extends Component {
-  render() {
-    return <SQS />;
-  }
+    render() {
+        return <SQS />;
+    }
 }
 
 async function run() {
-  const screen = blessed.screen({
-    smartCSR: true,
-    title: "Janelinha",
-    autoPadding: true,
-  });
+    const screen = blessed.screen({
+        smartCSR: true,
+        autoPadding: true,
+        title: "Janelinha",
+    });
 
-  // const list = blessed.list({
-  //     width: '50%',
-  //     height: '50%',
-  //     top: 'center',
-  //     left: 'center',
-  //     items: await SQS.fetchQueues(),
-  //     border: {
-  //         type: 'line'
-  //     },
-  //     interactive: true,
-  //     mouse: true,
-  //     keys: true
-  // })
-
-  // list.on('select item', (item) => {
-  //     console.log(item.content)
-  // })
-
-  // screen.append(list)
-
-  screen.key(["escape", "q", "C-c"], () => process.exit(0));
-
-  const component = render(<App />, screen);
+    screen.key(["escape", "q", "C-c"], () => process.exit(0));
+    render(<App />, screen);
 }
 
 run();
